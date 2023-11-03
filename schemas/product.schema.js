a38c505993d35be61bd46bee5cec9ee5c2bb3fbe
jeongseon0 <86090167+jeongseon0@users.noreply.github.com>
2023-11-03 20:14:15 +0900

    변수명 과제 예시와 같이 변경

--- schemas/product.schema.js
+++ schemas/product.schema.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 // 상품 정보: 상품명, 작성 내용, 작성자명, 비밀번호, 상품 상태(판매중 or 판매완료), 작성 날짜
 const productsSchema = new mongoose.Schema({
   // 상품 ID: 숫자로 된, 필수적인, 고유한 요소
-  productId: {
+  _id: {
     type: String,
     required: true,
     unique: true,
@@ -23,13 +23,18 @@ const productsSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  // 상품 상태: 문자로 된, 필수적인
+  status:{
+    type: String,
+    required: true,
+  },
   // 비밀번호: 숫자로 된, 필수적인
   password: {
     type: Number,
     required: true,
   },
   // 작성 날짜: 문자? 숫자? 객체도 가능할까?, 필수적인
-  date: {
+  createdAt: {
     type: Object,
     required: true,
   },
