803b24c632bd276cb2cad134aeb60ba5c9aef8e1
jeongseon0 <86090167+jeongseon0@users.noreply.github.com>
2023-11-06 20:32:27 +0900

    env 사용하기

--- schemas/index.js
+++ schemas/index.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose');
+require('dotenv').config()
 
+const id = process.env.MONGO_DB_ID;
+const pw = process.env.MONGO_DB_PW;
 
 const connect = () => {
-  mongoose.connect('mongodb://127.0.0.1:27017/sun_market', {dbName:"node_lv1"})
+  mongoose.connect(`mongodb+srv://${id}:${pw}@cluster0.ffquotr.mongodb.net/`, {dbName:"seon_market_db"})
   .then(() => console.log("MongoDB Connection Success"))
   .catch(error => console.log("MongoDB Connection Error", error));
 };
